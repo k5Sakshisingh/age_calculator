c72b25f88d49d217bfa37d526e1623a18da7b409
k5Sakshisingh <168255004+k5Sakshisingh@users.noreply.github.com>
2024-09-18 11:51:57 +0530

    Update index.js
    d3 to y3 (mistake)

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function calculateAge(){
         y3--;
       }
     //  console.log(y3,m3,d3);
-    result.innerHTML = ` You are  <span>${d3}</span> years <span>${m3}</span> months and <span>${d3}</span> days old `;
+    result.innerHTML = ` You are  <span>${y3}</span> years <span>${m3}</span> months and <span>${d3}</span> days old `;
 
 
 }    
